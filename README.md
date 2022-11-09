1e3f912d2186992001e92f14d8b74e22af26122a
rn7s2 <mail@rn7s2.cn>
2022-11-09 12:37:14 +0800

    basic functionality.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 A simple static file server.
 
 Set up a static file server in 10 secs!
+
+## License
+```
+Copyright © 2022 rn7s2 mail@rn7s2.cn
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the LICENSE file for more details.
+```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,57 @@
 use actix_files as fs;
 use actix_web::{App, HttpServer};
+use clap::{arg, command, Parser};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// e.g. "/static/"
+    #[arg(long, short)]
+    endpoint: Option<String>,
+
+    /// e.g. "."
+    #[arg(long, short)]
+    dir: Option<String>,
+
+    /// e.g. 8080
+    #[arg(long, short)]
+    port: Option<u16>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(fs::Files::new("/static", ".").show_files_listing()))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let args = Args::parse();
+
+    let (endpoint, dir, port) = (
+        match args.endpoint {
+            Some(v) => {
+                if !v.ends_with("/") {
+                    v + "/"
+                } else {
+                    v
+                }
+            }
+            None => "/".to_owned(),
+        },
+        match args.dir {
+            Some(v) => v,
+            None => ".".to_owned(),
+        },
+        match args.port {
+            Some(v) => v,
+            None => 8080,
+        },
+    );
+
+    println!(
+        "Static file server serving on: http://127.0.0.1:{}{}",
+        port, endpoint
+    );
+
+    HttpServer::new(move || {
+        App::new().service(fs::Files::new(&endpoint, &dir).show_files_listing())
+    })
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
 }
